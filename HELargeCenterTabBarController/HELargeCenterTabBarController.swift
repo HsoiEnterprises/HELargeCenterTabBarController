e229e4097562072638b85bf1aa4a128478a9f177
John C. Daub <hsoi@hsoi.com>
2015-09-20 11:21:19 -0500

    flesh out documentation

--- HELargeCenterTabBarController/HELargeCenterTabBarController.swift
+++ HELargeCenterTabBarController/HELargeCenterTabBarController.swift
@@ -30,6 +30,8 @@
 //  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 //  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
 
 import UIKit
 
@@ -88,14 +90,44 @@ class HELargeCenterTabBarController: UITabBarController {
     }
     
     
+    /// Reference to the center button. Marked optional, but generally should exist (else what's the point?).
     private weak var centerButton: UIButton?
+    
+    /// Reference to the "selected" version of the button. Marked optional, but generally should exist (else what's the point?).
     private var buttonImageSelected: UIImage?
+    
+    /// Reference to the "unselected" version of the button. Marked optional, but generally should exist (else what's the point?).
     private var buttonImageUnselected: UIImage?
+    
+    /// Reference to the action target. Optional, as the button doesn't have to have a target-action behavior.
     private var buttonTarget: AnyObject?
+    
+    /// Reference to the action selector. Optional, as the button doesn't haev to have a target-action behavior.
     private var buttonAction: Selector?
+    
+    /// Does tapping/selecting the center button allow the tab controller to actually switch to that tab or not? Generally if set to false, the target-action should be set so as to have some response to the button tap.
     private var allowSwitch: Bool = true
     
+    
+    /**
+    Sets the center tab/button to the given parameters.
+    
+    The center "tab" is really a UIButton, which will be sized to the dimensions of the given unselected image. The exact size of the image is up to you. In choosing a size, consider of course the height should extend slightly above the tab bar (but not too much so as to obscure content), and the width should be wide enough to cover most of the underlying tab bar button, but that some of the tab bar button will be exposed (depending upon device, tab bar width, device orientation, OS version, etc.).
+    
+    The image provides the whole content -- cosmetics, labels, borders, etc.. Ideally the selected and unselected images should be the same size, content, and general appearance, just that one will look selected (e.g. brighter) and the other will look unselected (e.g. more subdued). This will allow for seemless UI transitions between the two states.
+    
+    Because the center tab is actually a button, a target/action can be optionally provided so when the center tab is selected it will execute the target/action (as well as selecting the tab).
+    
+    The `allowSwitch` option provides control over whether taps on the tab/button will actually switch tabs or not. If true (default), taps on the button/tab will switch to make the center ViewController the selected/visible ViewController. If false, taps on the button/tab will not select the center ViewController. This allows the center button/tab to be used exclusively as a button (thus you will want to set the target and action), to give the center tab/button alternative behavior (e.g. perhaps instead of switching tabs, it presents a ViewController modally over the whole screen).
+    
+    - parameter unselectedImage: UIImage for the center button/tab, unselected state.
+    - parameter selectedImage:   UIImage for the center button/tab, selected state.
+    - parameter target:          Optional action target to message when tapping the center button/tab.
+    - parameter action:          Optional target selector to execute when tapping the center button/tab.
+    - parameter allowSwitch:     If true, taps on the center button/tab will select that tab. If false, taps will not select (but target-action will still be executed).
+    */
     public func addCenterButton(unselectedImage unselectedImage: UIImage, selectedImage: UIImage, target: AnyObject? = nil, action: Selector? = nil, allowSwitch: Bool = true) {
+        assert((target == nil && action == nil) || (target != nil && action != nil))
         assert(delegate === self, "HELargeCenterTabBarController must be its own delegate")
         delegate = self
         
@@ -124,6 +156,11 @@ class HELargeCenterTabBarController: UITabBarController {
     }
 
 
+    /**
+    HELargerCenterTabBarController must handle the taps on the centerButton. If `allowSwitch`, it will set the `selectedViewController` to the `centerViewController`. If the `buttonTarget` and `buttonAction` are set, execute them.
+    
+    - parameter sender: The sender of the action.
+    */
     @IBAction private func centerButtonAction(sender: AnyObject) {
         if allowSwitch {
             selectedViewController = centerViewController
